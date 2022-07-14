92e8d4ef6357deff10354789f20edf88530b1508
Caio Cesar <ahcaio@hotmail.com>
2022-07-14 08:03:21 -0300

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Caio Cesar Alves de Limeira on 7/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
