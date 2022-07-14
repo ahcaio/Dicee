70ed0256f015193a1daec6d36d4081034bc2bb30
Caio Cesar <ahcaio@hotmail.com>
2022-07-14 14:06:41 -0300

    Create Dicee game Interface Builder Create IBOutlets and IBAction

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix") // altera a imagem da ImageView
+        diceImageView2.image = UIImage(named: "DiceThree")
+
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("O botão foi pressionado")
+    }
 }
-
