ff4957c97f0ae35118f4b3ee275a035841898999
Caio Cesar <ahcaio@hotmail.com>
2022-07-14 19:21:07 -0300

    Refatoração do código

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,26 +12,14 @@ class ViewController: UIViewController {
     //Criar o array que armazena as imagens dos dado
     let dados = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"), UIImage(named: "DiceFour"),UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
     
-
-
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        diceImageView1.image = UIImage(named: "DiceOne") // altera a imagem da ImageView
-        diceImageView2.image = UIImage(named: "DiceOne")
-
-    }
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //Sortear um número do dado
-        let randomDice1 = Int.random(in: 0..<dados.count)
-        let randomDice2 = Int.random(in: 0..<dados.count)
-        
         //Exibir o dado sorteado
-        diceImageView1.image = dados[randomDice1]
-        diceImageView2.image = dados[randomDice2]
+        diceImageView1.image = dados[Int.random(in: 0..<dados.count)]
+        diceImageView2.image = dados[Int.random(in: 0..<dados.count)]
 
     }
 }
