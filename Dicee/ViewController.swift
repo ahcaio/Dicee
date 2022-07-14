76d31af4d46715acf314269639d73c4c4bf70ea6
Caio Cesar <ahcaio@hotmail.com>
2022-07-14 14:52:10 -0300

    Criação do array que armazena as imagens de cada lado do dado Criação das variáveis que sorteiam o número de cada dado Exibição do dado após ação de apertar o botão

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,19 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    //Criar o array que armazena as imagens dos dado
+    let dados = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"), UIImage(named: "DiceFour"),UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    
+
+
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageView1.image = UIImage(named: "DiceSix") // altera a imagem da ImageView
-        diceImageView2.image = UIImage(named: "DiceThree")
+        diceImageView1.image = UIImage(named: "DiceOne") // altera a imagem da ImageView
+        diceImageView2.image = UIImage(named: "DiceOne")
 
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("O botão foi pressionado")
+        //Sortear um número do dado
+        let randomDice1 = Int.random(in: 0..<dados.count)
+        let randomDice2 = Int.random(in: 0..<dados.count)
+        
+        //Exibir o dado sorteado
+        diceImageView1.image = dados[randomDice1]
+        diceImageView2.image = dados[randomDice2]
+
     }
 }
